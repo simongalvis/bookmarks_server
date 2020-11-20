04fffafdbc3e0c9c17414b8e943e2b4c4368f4f5
Simon Galvis <simongalvis9@gmail.com>
2020-11-20 18:08:58 -0500

    Add seed migration and successfully execute migration

--- migrations/001.do.create_boomarks.sql
+++ migrations/001.do.create_boomarks.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS bookmarks;
+DROP TYPE IF EXISTS star_rating;
+
 CREATE TYPE star_rating AS ENUM ('1', '2', '3', '4', '5');
 
 CREATE TABLE bookmarks (
@@ -5,6 +8,6 @@ CREATE TABLE bookmarks (
   title TEXT NOT NULL,
   url TEXT NOT NULL,
   description TEXT,
-  rating star_rating NOT NULL,
+  rating star_rating NOT NULL
   
 );
